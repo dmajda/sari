1554a7235d22b575bf8f649bcbf8bb2aabc4b896
David Majda <david@majda.cz>
2025-06-26 12:29:18 +0200

    Run `cargo fmt`

--- benches/eval_benches.rs
+++ benches/eval_benches.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use criterion::{BenchmarkId, Criterion, Throughput, criterion_group, criterion_main};
 use std::fmt;
 
 fn generate_expr(depth: u32) -> String {
